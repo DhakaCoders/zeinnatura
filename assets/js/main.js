79fe8e88669833320c58885ddd536577c848a1b2
Milon9787 <56488424+Milon9787@users.noreply.github.com>
2020-01-21 02:13:34 +0600

    blog page responsive done

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,6 +54,34 @@ if( $('.hm-product-slider').length ){
       slidesToScroll: 1,
       prevArrow: $('.productsliderarrows .leftArrow'),
       nextArrow: $('.productsliderarrows .rightArrow'),
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
 }
 
