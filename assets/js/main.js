3b0aac550e0f7fec5d0ecbf37e9152453de30377
shantoztech <45969365+shantoztech@users.noreply.github.com>
2020-01-22 20:12:23 +0600

    fixing

--- assets/js/main.js
+++ assets/js/main.js
@@ -308,28 +308,37 @@ if( $('.main-slider').length ){
 //slick slider
 
 if( $('.specialProductsSlider').length ){
-    $('.specialProductsSlider').slick({
-      dots: true,
-      arrows:true,
-      infinite: false,
-      speed: 300,
-      slidesToShow: 2.73,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1.7
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1.3
-          }
+  $('.specialProductsSlider').slick({
+    dots: true,
+    arrows:true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2.73,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2.73,
+          slidesToScroll: 1
         }
-      ]
-    });
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 }
 
 
